0ab2d53e4ab65223a090ee0ad7acaceed7742fb9
Samuel Meshoyrer <sam@meshoyrer.com>
2018-06-22 12:44:43 -0400

    added casename counting

--- src/count.py
+++ src/count.py
@@ -5,10 +5,21 @@ if __name__ == "__main__":
     zip_content = pd.read_csv(os.path.join(repo_dir, content_fn))
     states = zip_content.loc[zip_content["type"]=="states"] 
 
-    counts = {}
+    file_counts = {}
+    fullCaseName_counts = {}
     for zip_file in get_all_state_zips(): 
-        num = zip_file[:-4] 
-        with open(f"../tags/{num}_tags.json") as injson:
-            curr_tags = json.load(injson)
+        num = zip_file[:-4]
+        try:
+            with open(f"../tags/{num}_tags.json") as injson:
+                curr_fullCaseName = 0
+                curr_tags = json.load(injson)
+                file_counts[zip_file] = len(curr_tags) 
+                for fn, tags in curr_tags.items():
+                    curr_fullCaseName = tags.get("fullCaseName",0)
+                fullCaseName_counts[zip_file] = curr_fullCaseName
+        except FileNotFoundError:
+            pass
 
+    states["file_count"] = states["zip"].map(file_counts)
+    states["fullCaseName_count"] = states["zip"].map(fullCaseName_counts)
 
