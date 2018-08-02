d845a768be0f29b958d8dcba2193d4f8b90be570
Samuel Meshoyrer <sam@meshoyrer.com>
2018-08-02 00:20:22 -0400

    updated LNParser

--- src/LNParser.py
+++ src/LNParser.py
@@ -17,6 +17,7 @@ from xmljson import Yahoo
 from pathlib import Path
 from xml.etree import ElementTree
 from math import floor
+from parse_cases import parse_case
 
 repo_dir = os.path.expanduser(r"~/LexisNexisCorpus")
 zip_dir = os.path.expanduser(r"~/data/content-zip/content")
@@ -26,7 +27,7 @@ courtCaseRE = r"<courtCaseDoc.*?/courtCaseDoc>"
 
 xmlConverter = Yahoo()
 progress = [i/10 for i in range(11)]
-errors = []
+errorfn = "errors.txt" 
 
 def parseDir(mongoCollection, pathToSourceDir):
 	sourceDir = Path(pathToSourceDir)
@@ -34,7 +35,13 @@ def parseDir(mongoCollection, pathToSourceDir):
 	currProgress = set([floor(i * len(files)) for i in progress])
 	for i, f in enumerate(files): 
 		for parsedCase in parseFile(f):
-			mongoCollection.insert_one(parsedCase)
+			try: 
+				#print(parsedCase)
+				mongoCollection.insert_one(parsedCase)
+			except Exception as e: 
+				with open(errorfn, 'a') as eFile:
+					print("upload error: " + str(f))
+					eFile.write(','.join((str(f.absolute()), "upload error")))
 		if i in currProgress: 
 			print("{0}/{1} files done in {2}".format(i, len(files), pathToSourceDir))
 
@@ -43,26 +50,26 @@ def parseFile(pathFile):
 	with pathFile.open() as infile: 
 		for m in re.findall(courtCaseRE, infile.read()): 
 			try: 
-				caseXml = ElementTree.fromstring(m)  
-				caseDict = xmlConverter.data(caseXml)
-				caseDict = json.loads(json.dumps(caseDict).replace('.','_'))
-				parsedCases.append(caseDict)
-			except Exception as e: 
-				print("Error occurred on {0} - {1}".format(pathFile.resolve(), e.message))
-				errors.append((pathFile.resolve(), e.message))
+				parsedCase = parse_case(m) 
+				parsedCases.append(parsedCase) 
+			except Exception as e:
+				with open(errorfn,'a') as eFile: 
+					print("parse error: " + str(pathFile))
+					print(repr(e))
+					eFile.write(','.join((str(pathFile.absolute()), "parse error")))
 	return parsedCases
-	
 
 def uploadDir(mongoCollection, pathToSourceDir): 
 	parseDir(mongoCollection, pathToSourceDir)	
 
 if __name__ == "__main__": 
 	zipfiles = [z for z in get_all_state_zips() if z.endswith(".zip")]
-	
 	client = pymongo.MongoClient()
 	db = client.LexisNexis
 	mongoCollection = db["cases"]
-	for zf in zipfiles[:len(zipfiles)//10]:
+	for i, zf in enumerate(zipfiles):
+		print("--------------------------")
+		print("Starting zip file {0}/{1}".format(i, len(zipfiles)))
 		print("Copying", zf)
 		copy_zip_file(zf)
 		print("Unzipping", zf)
@@ -72,69 +79,6 @@ if __name__ == "__main__":
 		uploadDir(mongoCollection, os.path.join(repo_dir, 'content', num))
 		print("Deleting", zf)
 		delete_zip_file(zf)
-		print("Deleting zip dir")
+		print("Deleting zip dir "+ zf)
 		delete_unzipped_folder(os.path.join(repo_dir,'content',num))
-	with open("errors.json",'w') as outfile: 
-		json.dump(outfile, errors)
-
-
-'''
-class LNParser(object):
-    
-    def __init__(self, zip_dir, zip_files, repo_dir):
-        self.repo_dir = repo_dir
-        self.zip_dir = zip_dir
-        self.zip_files = zip_files
-        
-
-    def copy_and_unzip(zipfn):
-        print("copying", zipfn)
-        copy_zip_file(zipfile)
-        print("unzipping", zipfn)
-        unzip_file(zipfile)
-
-    def delete_zip(zipfn):
-        print("deleting", zipfn)
-        delete_zip_file(zipfn)
-        unzip_dir = os.path.join(repo_dir,'content',zipfn[:-4])
-        print("deleting",unzip_dir)
-        delete_unzipped_folder(unzip_dir)
-
-    def demo_extract_tag(self, tagName, save = False):
-        return self._extract_tag_from_files(tagName, list(self.zip_files[0]), save)
-
-    def extract_tag_from_all_files(self,tagName, save=False):
-        return self._extract_tag_from_files(tagName, self.zip_files, save)
-
-    def _extract_tag_from_files(self, tagName, zip_files, save = False):
-        tag_re = "<{}.*?</{}>"
-        tag = defaultdict(lambda: defaultdict(list))
-        content_dir = os.path.join(self.repo_dir, 'content')
-        errors = []  
-        for zipfn in zip_files:
-            try:
-                LNParser.copy_and_unzip(zipfn)
-                
-                num = zipfn[:-4]
-                unzipped_content_dir = os.path.join(content_dir, num)
-                all_fns = os.listdir(unzipped_content_dir)
-                num_all_fns = len(all_fns)
-                for fn in all_fns:
-                    fn_fullPath = os.path.join(unzipped_content_dir, fn)
-                    if os.path.isfile(fn_fullPath): 
-                        with open(fn_fullPath) as in_fp: 
-                            # TODO: avoid full file read()
-                            for match in re.findall(tag_re, infile.read())
-                                tag[zipfn][fn].append(str(match))
-            except Exception as e:
-                errors.append((zipfn, e.message))
-            
-            delete_zip(zipfn)
-
-        if save: 
-            json.dumps(tag, os.path.join(self.repo_dir,'tags',f"{tagName}.json"))
-            json.dumps(errors, os.path.join(self.repo_dir,'errors.json'))
-
-        return tag, errors
-'''
 
--- src/parse_cases.py
+++ src/parse_cases.py
@@ -1,9 +1,15 @@
 import re
 import xml.etree.ElementTree as ET
+from collections import defaultdict
+
+source = "}source"
 
 def parse_case(case_string):
 	parsed = dict() 
 	judges = set()
+	counselors = set() 
+	citations = list() 
+	paginationSchemes = set() 
 	xmlTree = ET.fromstring(case_string) 
 	for t in xmlTree.iter(): 
 		if t.tag == "fullCaseName":
@@ -24,8 +30,30 @@ def parse_case(case_string):
 			parsed["opinion"] = t.attrib
 		elif t.tag == "caseOpinionBy":
 			parsed["caseOpinionBy"] = t.text
-		elif t.tag == "nameText":
-			judges.add(t.text)
+		elif t.tag == "judge":
+			for sub_t in t.iter(): 
+				if sub_t.tag == "nameText":
+					judges.add(sub_t.text)
+		elif t.tag == "counselor":
+			for sub_t in t.iter(): 
+				if sub_t.tag == "nameText":
+					counselors.add(sub_t.text) 
+		elif t.tag == "keyValue":
+			citation = ''.join(t.attrib["value"].split())
+			citations.append(citation)
+		elif t.tag == "decisionDate":
+			t.attrib["fullDate"] = ''.join((t.attrib["year"],t.attrib["month"],t.attrib["day"]))
+			parsed["decisionDate"] = t.attrib
+		elif t.tag == "paginationScheme": 
+			paginationSchemes.update((v for k,v in t.attrib.items()))
+		elif t.tag[-len(source):] == source: 
+			parsed["productContentSetIdentifier"] = t.text
 	if judges: 
 		parsed["judges"] = list(judges)
-		
+	if citations:
+		parsed["citations"] = citations
+	if counselors: 
+		parsed["counselors"] = list(counselors) 
+	if paginationSchemes: 
+		parsed["paginationSchemes"] = list(paginationSchemes)
+	return parsed 		
